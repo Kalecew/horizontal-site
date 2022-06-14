db8b64671f0af33b772468e2f31673f94e753a04
Kalecew <48532006+Kalecew@users.noreply.github.com>
2022-06-14 14:47:41 +0300

    backup adaptive

--- src/components/Advantages/PopupAdvantages/DirectoryAdvantages/Pagination/Pagination.jsx
+++ src/components/Advantages/PopupAdvantages/DirectoryAdvantages/Pagination/Pagination.jsx
@@ -2,37 +2,37 @@ import s from './Pagination.module.css'
 
 const Pagination = ({countPages, currentPage, changePage}) => {
 	function* renderCircle()	{
-    for(let i = 1; i <= countPages; i++) {
-    	if(i === currentPage) {
-    		yield (
-        	<li className={s.item} key={i}>
-		        <button className={`${s.link}  ${s.linkCircle} ${s.linkCircleActive}`}>{i}</button>
-		      </li>
-		    )
-    	} else {
-    		yield (
-        	<li className={s.item} key={i}>
-		        <button className={`${s.link}  ${s.linkCircle}`} onClick={() => changePage(i)}>{i}</button>
-		      </li>
-		    )
-    	}
-    }   	
+		for(let i = 1; i <= countPages; i++) {
+			if(i === currentPage) {
+				yield (
+					<li className={s.item} key={i}>
+						<button className={`${s.link}  ${s.linkCircle} ${s.linkCircleActive}`}>{i}</button>
+					</li>
+				)
+			} else {
+				yield (
+					<li className={s.item} key={i}>
+						<button className={`${s.link}  ${s.linkCircle}`} onClick={() => changePage(i)}>{i}</button>
+					</li>
+				)
+			}
+		}   	
 	}
 	return(
 		<div className={s.pagination}>
-	    <ul className={s.list}>
+			<ul className={s.list}>
 
-	      <li className={s.item}>
-	        <button className={`${s.link} ${s.linkPrev}`} onClick={() => changePage(currentPage - 1)}>&lt;</button>
-	      </li>
+				<li className={s.item}>
+					<button className={`${s.link} ${s.linkPrev}`} onClick={() => changePage(currentPage - 1)}>&lt;</button>
+				</li>
 
-	      {[...renderCircle()]}
+				{[...renderCircle()]}
 
-	      <li className={s.item}>
-	        <button className={`${s.link} ${s.linkNext}`} onClick={() => changePage(currentPage + 1)}>&gt;</button>
-	      </li>
-	    </ul>
-	  </div>
+				<li className={s.item}>
+					<button className={`${s.link} ${s.linkNext}`} onClick={() => changePage(currentPage + 1)}>&gt;</button>
+				</li>
+			</ul>
+		</div>
 	)
 }
 
